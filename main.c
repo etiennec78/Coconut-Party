ae6e14694582cf4e4d700d42576fc62ca368f20c
Etienne <etienne.coriou@free.fr>
2025-04-24 22:59:16 +0200

    Restore main.c
    A debug file was accidentally uploaded with commit #99e2748

--- main.c
+++ main.c
@@ -20,21 +20,13 @@ void createGame(Game *game, int width, int height, unsigned int seed, int minPat
 
 int main() {
     Game game;
-    // unsigned int seed = 10; // time(NULL);
+    unsigned int seed = time(NULL);
     int minPathLength = 30;
     int maxPathLength = 200;
 
-
-    for (int i=0; i<10; i++) {
-        unsigned int seed = time(NULL);
-        printf("seed %d\n", seed);
-        createGame(&game, WIDTH, HEIGHT, seed, minPathLength, maxPathLength);
-        printGame(&game);
-    }
-
-    // createGame(&game, WIDTH, HEIGHT, seed, minPathLength, maxPathLength);
-    //
-    // printGame(&game);
+    createGame(&game, WIDTH, HEIGHT, seed, minPathLength, maxPathLength);
+    
+    printGame(&game);
 
     freeTerrain(game.terrain);
     free(game.path.tab);
