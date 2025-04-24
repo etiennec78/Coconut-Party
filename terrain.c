e926e0b563544812acd547affc39c73019fc7f80
etiennec78 <etienne.coriou@free.fr>
2025-04-25 00:10:23 +0200

    Code refractor: Renamed variables

--- terrain.c
+++ terrain.c
@@ -32,23 +32,23 @@ char** createTerrain(int width, int height) {
     int x0 = width / 2;
     int y0 = height / 2;                        
 
-    float a = 0.75 * width / 2;
-    float b = 0.75 * height / 2;                 // Calculation of the horizontal and vertical half axis of ellipse
+    float ray1 = 0.75 * width / 2;
+    float ray2 = 0.75 * height / 2;                 // Calculation of the horizontal and vertical half axis of ellipse
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    for (int x = 0; x < height; x++) {
+        for (int y = 0; y < width; y++) {
              
-            float value = ((j - x0) * (j - x0)) / (a * a) + ((i - y0) * (i - y0)) / (b * b)
+            float ellipse = ((y - x0) * (y - x0)) / (ray1 * ray1) + ((x - y0) * (x - y0)) / (ray2 * ray2)
                         + (rand() % 101)  / 1000;  // Calculation of the equation of the ellipse with a margin of +- 0.1
 
-            if (value < 1.0) {
+            if (ellipse < 1.0) {
                 if (rand() % 2 == 0) {
-                    terrain[i][j] = 0;
+                    terrain[x][y] = 0;
                 } else {
-                    terrain[i][j] = 1;
+                    terrain[x][y] = 1;
                 }  
             } else {
-                terrain[i][j] = 2;  
+                terrain[x][y] = 2;
             }
         }
     }
