9eb380b7ac11894baf52cd14a2670457e01143d6
MartinJdy <jeudy.martin14@gmail.com>
2025-04-25 17:37:56 +0200

    Added 4 types of maps with new emojis

--- display.c
+++ display.c
@@ -3,22 +3,87 @@
 
 #include "display.h"
 
-void printTerrain(char** terrain, int width, int height) {
-    for (int i=0; i<height; i++) {
-        for (int j=0; j<width; j++) {
-            switch (terrain[i][j]) {
-                case 0:
-                    printf("🌳");
-                    break;
-                case 1:
-                    printf("🌴");
-                    break;
+void printTerrain(char** terrain, int width, int height, int season) {
+    switch(season){
+        case 1:
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    switch (terrain[i][j]) {
+                        case 0: 
+                            printf("🌿");
+                            break;
+                        case 1: 
+                            printf("🌸"); 
+                            break;
+                        case 2: 
+                            printf("💧"); 
+                            break;
+                    }
+                }
+                printf("\n");
             }
-        }
-        printf("\n");
+            break;
+
+        case 2:
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    switch (terrain[i][j]) {
+                        case 0: 
+                            printf("🌳");
+                            break;
+                        case 1: 
+                            printf("🌴"); 
+                            break;
+                        case 2: 
+                            printf("🌊"); 
+                            break;
+                    }
+                }
+                printf("\n");
+            }
+            break;
+
+        case 3:
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    switch (terrain[i][j]) {
+                        case 0: 
+                            printf("🍁");
+                            break;
+                        case 1: 
+                            printf("🌱"); 
+                            break;
+                        case 2: 
+                            printf("💦"); 
+                            break;
+                    }
+                }
+                printf("\n");
+            }
+            break;
+
+        case 4:
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    switch (terrain[i][j]) {
+                        case 0: 
+                            printf("🌲");
+                            break;
+                        case 1: 
+                            printf("🪵"); 
+                            break;
+                        case 2: 
+                            printf("🧊"); 
+                            break;
+                    }
+                }
+                printf("\n");
+            }
+            break;
     }
 }
 
-void printGame(char** terrain, int width, int height) {
-    printTerrain(terrain, width, height);
+void printGame(char** terrain, int width, int height, int season) {
+    int season = 2;
+    printTerrain(terrain, width, height, season);
 }
